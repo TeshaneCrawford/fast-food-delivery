e151b81136d06cb83e19db8a1311bf22e9d571b9
Teshane Crawford <crawfordteshane@gmail.com>
2022-10-16 23:05:19 -0500

    complete, checking errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-
-import './App.css'
+import Layout from "./components/Layout/Layout";
 
 function App() {
-
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  )
+  return <Layout />;
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
   plugins: [react()]
 })
